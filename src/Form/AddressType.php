aa041b945e4850b967795bee5d520ac5427bf86c
Kévin Dargès <kevin.darges@gmail.com>
2021-09-14 14:24:59 +0200

    label

--- src/Form/AddressType.php
+++ src/Form/AddressType.php
@@ -8,18 +8,20 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use App\Entity\Ville;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class AddressType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('no')
+            ->add('no', IntegerType::class, [ 'label' => 'Numéro'])
             ->add('rue')
             ->add('villes', EntityType::class, [
+                'label' => 'Ville',
                 'class' => Ville::class,
 
-    //  nom de la ville property as the visible option string // mettre villes en tableau
+            //  nom de la ville property as the visible option string // mettre villes en tableau
                  'choice_label' => 'nom',
                  'multiple' => true,
             ])
