9447a11e651111afde29947d460bb92d9405faf6
badisbms <badisbms@gmail.com>
2021-09-15 21:10:51 +0200

    ajout creation d'annonce

---
+++ src/Form/AnnonceType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Annonce;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnonceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('date')
+            ->add('prix')
+            ->add('image')
+            // ->add('fk_user')
+            ->add('fk_ville')
+            ->add('categorie')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Annonce::class,
+        ]);
+    }
+}
