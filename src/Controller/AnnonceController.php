108b0dde44deac69b95369eb0d911b1a29b6b44a
Kévin Dargès <kevin.darges@gmail.com>
2021-09-16 15:05:15 +0200

    oui badis je push

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Annonce;
-use App\Entity\User;
 
 class AnnonceController extends AbstractController
 {
--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\Annonce;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
 
 class AnnonceType extends AbstractType
 {
@@ -16,7 +18,21 @@ class AnnonceType extends AbstractType
             ->add('description')
             ->add('date')
             ->add('prix')
-            ->add('image')
+            ->add('image', FileType::class, [
+                'label' => 'Image (png, jpg)',
+                'mapped' => false, // n'est pas associé au get/set
+                'required' => true, // required
+
+                'constraints' => [
+                    new Image([
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Insérez une image valide',
+                    ])
+                ]
+            ])
             // ->add('fk_user')
             ->add('fk_ville')
             ->add('categorie')
