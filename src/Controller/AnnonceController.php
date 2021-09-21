777ae6c35055d48aae1cdc0f69eb7b8280c8d9c9
Kévin Dargès <kevin.darges@gmail.com>
2021-09-21 14:20:42 +0200

    pagination

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -6,10 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Annonce;
-use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\AnnonceType;
-use App\Repository\AnnonceRepository;
 
 
 class AnnonceController extends AbstractController
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,28 +4,41 @@ namespace App\Controller;
 
 use App\Repository\AnnonceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="home")
      */
-    public function index(AnnonceRepository $annonceRepository): Response
+    public function index(AnnonceRepository $annonceRepository, Request $request, PaginatorInterface $paginator)
     {
+        $annonces = $paginator->paginate(
+            $donnees = $annonceRepository->findByExampleField(),
+            $request->query->getInt('page', 1),
+            9 // nb article par page
+        );
+
         return $this->render('home/index.html.twig', [
-            'annonces' => $annonceRepository->findByExampleField(),
+            'annonces' => $annonces,
         ]);
     }
 
       /**
      * @Route("/", name="/")
      */
-    public function home(AnnonceRepository $annonceRepository): Response
+    public function home(AnnonceRepository $annonceRepository, Request $request, PaginatorInterface $paginator)
     {
+        $annonces = $paginator->paginate(
+            $donnees = $annonceRepository->findByExampleField(),
+            $request->query->getInt('page', 1),
+            10 // nb article par page
+        );
+
         return $this->render('home/index.html.twig', [
-            'annonces' => $annonceRepository->findByExampleField(),
+            'annonces' => $annonces,
         ]);
     }
 }
